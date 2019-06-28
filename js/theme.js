1bd3408978c02895d33a1ab3f204ffa51d896637
CloudSaver <thecloudsaver@gmail.com>
2019-06-28 21:41:59 +0200

    Change theme color depending on mode..

--- js/theme.js
+++ js/theme.js
@@ -5,6 +5,9 @@ var lightMode = true;
 var light = "#ecf0f1";
 var dark = "#2c3e50";
 
+var theme_light = "#3498db";
+var theme_dark = "#6a89cc";
+
 mode_switch.addEventListener('click', function (event) {
     event.preventDefault();
     toggleMode();
@@ -14,11 +17,13 @@ function toggleMode() {
     if (lightMode) {
         root.style.setProperty('--primary', dark);
         root.style.setProperty('--secondary', light);
+        root.style.setProperty('--theme', theme_dark);
 
         lightMode = false;
     } else {
         root.style.setProperty('--primary', light);
         root.style.setProperty('--secondary', dark);
+        root.style.setProperty('--theme', theme_light);
 
         lightMode = true;
     }
